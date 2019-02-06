c27eeafdfaea9f2ff94b728755b03ca0105721a8
Emmett Young <emmettyoung92@gmail.com>
2019-02-06 21:51:11 +0800

    Add junit 5 module for domain tier

---
+++ domain/src/test/java/com/mettyoung/pethotel/SuiteTest.java
@@ -0,0 +1,13 @@
+package com.mettyoung.pethotel;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SuiteTest {
+
+    @Test
+    public void testMethod() {
+        assertEquals(2, 1 + 1);
+    }
+}
